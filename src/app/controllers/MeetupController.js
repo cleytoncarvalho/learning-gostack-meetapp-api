be170ea4cc2e91e298b605e0a690e78ec8c8c295
Cleyton Carvalho <cleyton.carvalho@outlook.com>
2019-07-12 18:49:53 -0300

    feature: Meetup manager

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,30 @@
+import * as Yup from 'yup';
+
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      file_id: Yup.number().required(),
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const user_id = req.userId;
+
+    const meetup = await Meetup.create({
+      ...req.body,
+      user_id,
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
